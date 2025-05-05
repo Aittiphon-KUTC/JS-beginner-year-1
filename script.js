b762436aaa2040b5b29296489ce04b89f26858ac
Aittiphon-KUTC <aittiphon.somsamer@gmail.com>
2025-05-05 22:00:42 +0700

    Revert "Merge pull request #1 from Aittiphon-KUTC/Readme-benzayngcup-"
    This reverts commit bbd54625cb48855ed0a9f8be0b9e18203561c1e6, reversing
    changes made to d503fd7a7dc607156a78be0043764b76aad5e0e5.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function changealert() {
     // คำสั่งในการดึงข้อมูลจาก HTML โดยระบุ ID มาเป้นตัวดำเนินการ
     document.getElementById("change-alert").value=""
     //เครียข้อมูลออกจากฟอร์มที่กรอกเข้ามา
+    //โดยที่จะต้องอ้างอิงข้อมูลก่อน
     alert(msg)
 }
 
